f71a1874eb240103c202ad6a5201fb01d822d54d
Francesc Leveque <francesc@leveque.es>
2025-07-22 17:37:37 +0000

    Stock name should be optional

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,6 @@ class Stock < ApplicationRecord
   has_many :radars, through: :radar_stocks
 
   validates :symbol, presence: true, uniqueness: true
-  validates :name, presence: true
 
   def self.last_added(limit = 10)
     order(created_at: :desc).limit(limit)
